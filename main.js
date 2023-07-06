08fe5828d040849dace6ef66f0f7e94fba8cff69
Marielysv <marielysvidal10@gmail.com>
2023-07-06 13:12:32 -0400

    Actualizar archivo main.js

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 
-    // Obtener los elementos de los botones
+    // Obteniendo los elementos de los botones
 const btnInicio = document.querySelector('.menu-item a[href="inicio"]');
 const btnContacto = document.querySelector('.menu-item a[href="contacto"]');
 
-// Agregar el evento click para cambiar el color al hacer clic en el botón "Inicio"
+// Agregando el evento click en el botón "Inicio"
 btnInicio.addEventListener('click', function() {
   btnInicio.style.backgroundColor = '#637373';
 });
 
-// Agregar el evento click para cambiar el color al hacer clic en el botón "Contacto"
+// Agregando el evento para cambiar color en el botón "Contacto"
 btnContacto.addEventListener('click', function() {
   btnContacto.style.backgroundColor = '#637373';
 });
 
 
-// Obtener el elemento del botón "Descargar"
+// Obteniendo el elemento del boton
 const btnDescargar = document.querySelector('.btn');
 
-// Agregar el evento mouseover para cambiar el color al pasar el mouse por encima del botón "Descargar"
+// Agregar el evento para cambiar el color del botón "Descargar"
 btnDescargar.addEventListener('mouseover', function() {
   btnDescargar.style.backgroundColor = '#679b9b';
 });
 
-// Agregar el evento mouseout para restaurar el color al quitar el mouse del botón "Descargar"
+// Agregar el evento para restaurar el color nuevamentae del botón "Descargar"
 btnDescargar.addEventListener('mouseout', function() {
   btnDescargar.style.backgroundColor = '#ff9a76';
 });
